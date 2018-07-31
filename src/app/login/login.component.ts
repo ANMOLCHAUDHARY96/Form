25e4c9903b16fa5d5cb57dd2e2981c88969356f4
anmol chaudhary96 <anmolchaudhary96@gmail.com>
2018-07-31 20:53:26 +0530

    fourth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,16 +13,17 @@ export class LoginComponent implements OnInit {
       fname: new FormControl('',[Validators.required,Validators.pattern("^[A-Za-z]+$")]),
       lname: new FormControl('',[Validators.required,Validators.pattern("^[A-Za-z]+$")]),
       gender: new FormControl('',[Validators.required,Validators.pattern("^[A-Za-z]+$")]),
-      contact: new FormControl('',[Validators.required,Validators.pattern("^(([0-9]*)|(([0-9]*)\.([0-9]*)))$")]),
+      contact: new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(12),Validators.pattern("^(([0-9]*)|(([0-9]*)\.([0-9]*)))$")]),
       email: new FormControl('',[Validators.required,Validators.email]),
       pass: new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(16)]),
       cpass: new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(16)]),
-      empId: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(4),Validators.pattern("^(([0-9]*)|(([0-9]*)\.([0-9]*)))$")])
+      empId: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(4),Validators.pattern("^(([0-9]*)|(([0-9]*)|([0-9]*)))$")])
     }
   )
 
   onclick(){
-    localStorage.setItem("fdata",JSON.stringify(this.form.value));
+    localStorage.setItem("data",JSON.stringify(this.form.value));
+    
   }
 
   constructor() { }
